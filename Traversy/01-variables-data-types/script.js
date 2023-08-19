7ffb15aa0424d598697cf51594ea3cc91769d626
nanyamax <ifunanyasunday7@gmail.com>
2023-08-19 17:23:33 +0300

    js

---
+++ Traversy/01-variables-data-types/script.js
@@ -0,0 +1,55 @@
+// string properties and methods
+// const s = 'hello world';
+// x = typeof s;
+// x = s.length;
+// // access value by key
+// x = s[1];
+
+// x = s.__proto__;
+// x = s.toLowerCase();
+
+// Challenge 1
+// // console.log(x);
+// const myString = 'developer';
+// let myNewString = myString[0].toUpperCase() + myString.substring(1);
+
+// console.log(myNewString);
+
+// Challenge 2
+// x = Math.round(Math.random() * 100);
+// y = Math.round(Math.random() * 50);
+// let sumOutput = x + y;
+// sumOutput = `${x} + ${y} = ${sumOutput}`;
+// console.log(sumOutput);
+
+// let differenceOutput = x - y;
+// differenceOutput = `${x} - ${y} = ${differenceOutput}`;
+// console.log(differenceOutput);
+
+// let productOutput = x * y;
+// productOutput = `${x} * ${y} = ${productOutput}`;
+// console.log(productOutput);
+
+// let quotientOutput = x / y;
+// quotientOutput = `${x} / ${y} = ${quotientOutput}`;
+// console.log(quotientOutput);
+
+// let rmOutput = x % y;
+// rmOutput = `${x} % ${y} = ${rmOutput}`;
+// console.log(rmOutput);
+
+// Challenge 3
+// const arr = [1, 2, 3, 4, 5];
+// arr.push(0, 6);
+// arr.sort().reverse();
+
+// console.log(arr);
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [5, 6, 7, 8, 9, 10];
+
+// const arr3 = arr1.concat(arr2.splice(1));
+// console.log(arr3);
+
+// Object literals
+// A common data structure that holds key/value pairs.
